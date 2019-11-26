aa9392e442253b2f38dc8f51718569948fe2b989
daanvanrobays <vanrobays.daan@hotmail.com>
2019-11-26 21:38:20 +0100

    fix build error

--- src/app/special-projects/special-projects.module.ts
+++ src/app/special-projects/special-projects.module.ts
@@ -26,7 +26,7 @@ const routes: Routes = [
     ],
     declarations: [
       BelmodoComponent,
-      AmfiFashionShowComponent
+      AmfiFashionShowComponent,
       VisualCultureComponent
     ],
     exports: [
